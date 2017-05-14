467b4bf0aa7df5ceb2bb477a0c028c57d2083938
glaux <mirmikh@gmail.com>
2017-05-14 12:31:06 +0300

    Add webpack CSS/SASS config

--- src/app.js
+++ src/app.js
@@ -1,1 +1,3 @@
-console.log("Hey there. This is webpack talking")
+const css = require('./app.scss');
+
+console.log("Hey there. This is webpack talking");
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,36 @@
 const path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js'
   },
+  module: {
+    rules: [
+      { test: /\.scss$/, 
+        use: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: ['css-loader', 'sass-loader'],
+          publicPath: '/dist' 
+        })
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Webpack learning project',
-      minify: {
-        collapseWhitespace: true
-      },
+      //minify: {
+        //collapseWhitespace: true
+      //},
       hash: true,
       template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
+    }),
+    new ExtractTextPlugin({
+      filename: "app.css",
+      allChunks: true
     })
   ]
 }
